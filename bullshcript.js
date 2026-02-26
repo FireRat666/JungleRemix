f71466475d87935501eef4bedb398684cca6267a
FireRat <27763388+FireRat666@users.noreply.github.com>
2026-02-26 21:00:06 +1100

    update bullshcript.js and index.html

--- bullshcript.js
+++ bullshcript.js
@@ -1,4 +1,5 @@
-// Enables Interaction for all the browser windows by HBR
+setTimeout(() => { 
+    // Enables Interaction for all the browser windows by HBR
 	  AFRAME.registerComponent("enable-interaction", { init: async function() { await window.AframeInjection.waitFor(this.el, "browser");
 			this.el.browser.ToggleInteraction(true) 			} });
 
@@ -10,3 +11,5 @@
     leaderboard.browser.ToggleKeyboard(0);
     console.log("Hopefully set browser page width and height");
   }, 12000); 
+
+  }, 8000); 
